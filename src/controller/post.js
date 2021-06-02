42a958384ed0139749a21ee876563b23282e5159
chwasiq0569 <wasiqabdullah222@gmail.com>
2021-06-01 23:11:42 -0700

    Created DeletePost Route

--- src/controller/post.js
+++ src/controller/post.js
@@ -32,3 +32,22 @@ module.exports.updatePost = async (req, res) => {
     res.status(500).json({ message: "Post not found" });
   }
 };
+
+module.exports.deletePost = async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (post) {
+    if (req.body.userId === post.userId) {
+      try {
+        console.log(post, req.body);
+        await post.deleteOne({ $set: req.body });
+        res.status(200).json({ message: "your Post is deleted Successfully!" });
+      } catch (err) {
+        res.status(403).json({ message: "Post not Deleted" });
+      }
+    } else {
+      res.status(502).json({ message: "You can only delete your Post" });
+    }
+  } else {
+    res.status(500).json({ message: "Post not found" });
+  }
+};
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const { createPost, updatePost } = require("../controller/post");
+const { createPost, updatePost, deletePost } = require("../controller/post");
 
 router.post("/create", createPost);
 router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
 
 module.exports = router;

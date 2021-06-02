7337b2e435e5fa66cbb4cd0b16efd77ceaebce9f
chwasiq0569 <wasiqabdullah222@gmail.com>
2021-06-01 23:49:57 -0700

    Created getposts route

--- src/controller/post.js
+++ src/controller/post.js
@@ -51,3 +51,35 @@ module.exports.deletePost = async (req, res) => {
     res.status(500).json({ message: "Post not found" });
   }
 };
+
+module.exports.likePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post) {
+      if (!post.likes.includes(req.body.userId)) {
+        await post.updateOne({ $push: { likes: req.body.userId } });
+        res.status(200).json({ message: "Post Liked" });
+      } else {
+        await post.updateOne({ $pull: { likes: req.body.userId } });
+        res.status(200).json({ message: "Post Disliked" });
+      }
+    } else {
+      res.status(200).json({ message: "Post not found" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports.getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post not found" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
-const { createPost, updatePost, deletePost } = require("../controller/post");
+const {
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+  getPost,
+} = require("../controller/post");
 
+router.get("/:id", getPost);
 router.post("/create", createPost);
 router.put("/:id", updatePost);
 router.delete("/:id", deletePost);
+router.put("/:id/like", likePost);
 
 module.exports = router;
